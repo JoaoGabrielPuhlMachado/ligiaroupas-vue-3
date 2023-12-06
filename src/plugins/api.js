f45a102c4c7859f4ca263ab6b84fdf839893dfac
joao <zala4453523@gmail.com>
2023-12-06 09:06:21 -0300

    V39.1 (botei api no local)

--- src/plugins/api.js
+++ src/plugins/api.js
@@ -3,13 +3,13 @@ import { useAuthStore } from "@/stores/auth";
 import { storeToRefs } from "pinia";
 // const MY_IP = import.meta.env.VITE_MY_IP;
 const api = axios.create({
-  baseURL: `https://ligiaroupas.3.us-1.fl0.io/api/`,
-  // baseURL: `http://0.0.0.0:19003/api/`,
+  // baseURL: `https://ligiaroupas.3.us-1.fl0.io/api/`,
+  baseURL: `http://0.0.0.0:19003/api/`,
 });
 axios.interceptors.request.use(
   (config) => {
-    config.baseURL = `https://ligiaroupas.3.us-1.fl0.io/api/`;
-    // config.baseURL = `http://0.0.0.0:19003/api/`;
+    // config.baseURL = `https://ligiaroupas.3.us-1.fl0.io/api/`;
+    config.baseURL = `http://0.0.0.0:19003/api/`;
     const authStore = useAuthStore();
     const { token } = storeToRefs(authStore);
     if (token.value !== undefined) {
